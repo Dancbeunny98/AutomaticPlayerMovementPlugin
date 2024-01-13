891c1bfdd771b6f935133a8c0bf1a44dff46e9da
Dancbeunny98 <jackworldppooii@gmail.com>
2024-01-14 00:02:54 +0300

    world list

--- src/main/java/ru/dancebunny98/AutomaticPlayerMovementPlugin.java
+++ src/main/java/ru/dancebunny98/AutomaticPlayerMovementPlugin.java
@@ -19,56 +19,56 @@ public class AutomaticPlayerMovementPlugin extends JavaPlugin implements Listene
 
     @Override
     public void onEnable() {
-        loadConfig();
-        getServer().getPluginManager().registerEvents(this, this);
+        loadConfig ();
+        getServer ().getPluginManager ().registerEvents (this, this);
     }
 
     private void loadConfig() {
-        File configFile = new File(getDataFolder(), "config.yml");
-        if (!configFile.exists()) {
-            saveDefaultConfig();
+        File configFile = new File (getDataFolder (), "config.yml");
+        if (!configFile.exists ()) {
+            saveDefaultConfig ();
         }
 
-        YamlConfiguration yamlConfig = YamlConfiguration.loadConfiguration(configFile);
-        pluginConfig = new PluginConfig();
+        YamlConfiguration yamlConfig = YamlConfiguration.loadConfiguration (configFile);
+        pluginConfig = new PluginConfig ();
 
-        pluginConfig.setWorldNames(yamlConfig.getStringList("worldNames"));
-        pluginConfig.setX(yamlConfig.getInt("x", 0));
-        pluginConfig.setY(yamlConfig.getInt("y", -60));
-        pluginConfig.setZ(yamlConfig.getInt("z", 0));
-        pluginConfig.setYaw((float) yamlConfig.getDouble("yaw", -180));
-        pluginConfig.setPitch((float) yamlConfig.getDouble("pitch", 0));
-        pluginConfig.setAllowedMaterials(yamlConfig.getStringList("allowedMaterials"));
-        pluginConfig.setMinY(yamlConfig.getDouble("minY", -63.0));
+        pluginConfig.setWorldNames (yamlConfig.getStringList ("worldNames"));
+        pluginConfig.setX (yamlConfig.getInt ("x", 0));
+        pluginConfig.setY (yamlConfig.getInt ("y", -60));
+        pluginConfig.setZ (yamlConfig.getInt ("z", 0));
+        pluginConfig.setYaw ((float) yamlConfig.getDouble ("yaw", -180));
+        pluginConfig.setPitch ((float) yamlConfig.getDouble ("pitch", 0));
+        pluginConfig.setAllowedMaterials (yamlConfig.getStringList ("allowedMaterials"));
+        pluginConfig.setMinY (yamlConfig.getDouble ("minY", -63.0));
     }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
-        Location playerLocation = player.getLocation();
+        Player player = event.getPlayer ();
+        Location playerLocation = player.getLocation ();
 
-        Material blockType = playerLocation.getBlock().getType();
-        if (pluginConfig.getAllowedMaterials().contains(blockType.toString()) || playerLocation.getY() < pluginConfig.getMinY()) {
-            teleportPlayer(player);
+        Material blockType = playerLocation.getBlock ().getType ();
+        if (pluginConfig.getAllowedMaterials ().contains (blockType.toString ()) || playerLocation.getY () < pluginConfig.getMinY ()) {
+            teleportPlayer (player);
         }
     }
 
     private void teleportPlayer(Player player) {
-        player.setNoDamageTicks(20);
+        player.setNoDamageTicks (20);
 
-        World world = player.getWorld();
+        World world = player.getWorld ();
 
-        List<String> worldNames = pluginConfig.getWorldNames();
-        String targetWorldName = worldNames.isEmpty() ? "world" : worldNames.get(0); // Используем первый мир из списка или "world" по умолчанию
-        Location teleportLocation = new Location(
-                Bukkit.getWorld(targetWorldName),
-                pluginConfig.getX(),
-                pluginConfig.getY(),
-                pluginConfig.getZ(),
-                pluginConfig.getYaw(),
-                pluginConfig.getPitch()
+        List<String> worldNames = pluginConfig.getWorldNames ();
+        String targetWorldName = worldNames.isEmpty () ? "world" : worldNames.get (0);
+        Location teleportLocation = new Location (
+                Bukkit.getWorld (targetWorldName),
+                pluginConfig.getX (),
+                pluginConfig.getY (),
+                pluginConfig.getZ (),
+                pluginConfig.getYaw (),
+                pluginConfig.getPitch ()
         );
 
-        player.teleport(teleportLocation);
+        player.teleport (teleportLocation);
     }
 }
